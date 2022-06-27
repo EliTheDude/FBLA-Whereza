5652a07679928b84279d17f555efab61360c027d
robotenthusiast <malak.elias.06@gmail.com>
2022-06-27 10:47:31 -0700

    V2

--- app/src/main/java/com/fbla/whereza/NewThirdActivity.kt
+++ app/src/main/java/com/fbla/whereza/NewThirdActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
@@ -14,6 +15,7 @@ import com.bumptech.glide.request.transition.Transition
 import com.fbla.whereza.databinding.ActivityMainBinding
 import com.fbla.whereza.databinding.NewThirdPageBinding
 import java.lang.Exception
+import javax.security.auth.callback.Callback
 
 
 class NewThirdActivity : AppCompatActivity() {
@@ -39,15 +41,33 @@ class NewThirdActivity : AppCompatActivity() {
 
     private fun populatePlaces(image: ImageView) {
         Log.d("DidIt1", glideGet("https://content.fortune.com/wp-content/uploads/2017/04/restuarant-exterior-red-robin-4749x2633.jpg?resize=750,500", image).toString())
-        val place1 = Places(
-            glideGet("https://content.fortune.com/wp-content/uploads/2017/04/restuarant-exterior-red-robin-4749x2633.jpg?resize=750,500", image),
-            "Red Robin",
-            "5.5 mi.",
-            "4.5",
-            "200",
-            "somewhere you know"
-        )
-        placeList.add(place1)
+
+        Glide.with(this)
+            .asBitmap()
+            .load("https://content.fortune.com/wp-content/uploads/2017/04/restuarant-exterior-red-robin-4749x2633.jpg?resize=750,500")
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    val place1 = Places(
+                        resource,
+                        "Red Robin",
+                        "5.5 mi.",
+                        "4.5",
+                        "200",
+                        "somewhere you know"
+                    )
+                    placeList.add(place1)
+
+                    binding.RecyclerView.apply {
+                        layoutManager = GridLayoutManager(applicationContext, 1)
+                        adapter = CardAdapter(placeList)
+                    }
+
+                    Log.d("DidIt56789", placeList.isEmpty().toString())
+                }
+                override fun onLoadCleared(placeholder: Drawable?) {
+                    TODO("Not yet implemented")
+                }
+            })
     }
 
 
@@ -55,7 +75,7 @@ class NewThirdActivity : AppCompatActivity() {
 
     private fun glideGet(imageUrl: String, image: ImageView): Bitmap? {
        var bitMapImage: Bitmap? = null
-       GlideApp.with(this)
+       Glide.with(this)
             .asBitmap()
             .load(imageUrl)
             .into(object : CustomTarget<Bitmap>() {
