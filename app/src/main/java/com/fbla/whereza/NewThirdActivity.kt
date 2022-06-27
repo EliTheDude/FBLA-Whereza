643400b8df9bed78eec07fee391724c2ec375aff
robotenthusiast <malak.elias.06@gmail.com>
2022-06-27 14:26:07 -0700

    V2

--- app/src/main/java/com/fbla/whereza/NewThirdActivity.kt
+++ app/src/main/java/com/fbla/whereza/NewThirdActivity.kt
@@ -20,6 +20,7 @@ import javax.security.auth.callback.Callback
 
 class NewThirdActivity : AppCompatActivity() {
 
+
     private lateinit var binding: NewThirdPageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,11 +28,7 @@ class NewThirdActivity : AppCompatActivity() {
         binding = NewThirdPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val image = findViewById<ImageView>(R.id.imageView)
-
-        populatePlaces(image)
-
-        val mainActivity = this
+        populatePlaces()
 
         binding.RecyclerView.apply {
             layoutManager = GridLayoutManager(applicationContext, 1)
@@ -39,56 +36,39 @@ class NewThirdActivity : AppCompatActivity() {
         }
     }
 
-    private fun populatePlaces(image: ImageView) {
-        Log.d("DidIt1", glideGet("https://content.fortune.com/wp-content/uploads/2017/04/restuarant-exterior-red-robin-4749x2633.jpg?resize=750,500", image).toString())
+    private fun populatePlaces() {
+        glideGet("https://content.fortune.com/wp-content/uploads/2017/04/restuarant-exterior-red-robin-4749x2633.jpg?resize=750,500") { bitmap ->
+            val place1 = Places(
+                bitmap,
+                "Red Robin",
+                "5.5 mi.",
+                "4.5",
+                "200",
+                "somewhere you know"
+            )
+            placeList.add(place1)
 
-        Glide.with(this)
-            .asBitmap()
-            .load("https://content.fortune.com/wp-content/uploads/2017/04/restuarant-exterior-red-robin-4749x2633.jpg?resize=750,500")
-            .into(object : CustomTarget<Bitmap>() {
-                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                    val place1 = Places(
-                        resource,
-                        "Red Robin",
-                        "5.5 mi.",
-                        "4.5",
-                        "200",
-                        "somewhere you know"
-                    )
-                    placeList.add(place1)
-
-                    binding.RecyclerView.apply {
-                        layoutManager = GridLayoutManager(applicationContext, 1)
-                        adapter = CardAdapter(placeList)
-                    }
-
-                    Log.d("DidIt56789", placeList.isEmpty().toString())
-                }
-                override fun onLoadCleared(placeholder: Drawable?) {
-                    TODO("Not yet implemented")
-                }
-            })
+            binding.RecyclerView.apply {
+                layoutManager = GridLayoutManager(applicationContext, 1)
+                adapter = CardAdapter(placeList)
+            }
+        }
     }
 
 
 
 
-    private fun glideGet(imageUrl: String, image: ImageView): Bitmap? {
-       var bitMapImage: Bitmap? = null
+    private fun glideGet(imageUrl: String, callback: (Bitmap?) -> Unit) {
        Glide.with(this)
             .asBitmap()
             .load(imageUrl)
             .into(object : CustomTarget<Bitmap>() {
                 override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                    bitMapImage = resource
-                    Log.d("DidIt2", bitMapImage.toString())
-                    image.setImageBitmap(resource)
+                    callback(resource)
                 }
                 override fun onLoadCleared(placeholder: Drawable?) {
                     TODO("Not yet implemented")
                 }
             })
-        Log.d("DidIt3", bitMapImage.toString())
-        return bitMapImage
     }
 }
